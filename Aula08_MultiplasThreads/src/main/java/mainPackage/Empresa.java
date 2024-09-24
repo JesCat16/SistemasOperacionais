c21b97674ce67fb1dc5323d31b6adc8d66a6f286
JesCat16 <123671844+JesCat16@users.noreply.github.com>
2024-09-23 22:28:21 -0300

    Add files via upload

--- Aula08_MultiplasThreads/src/main/java/mainPackage/Empresa.java
+++ Aula08_MultiplasThreads/src/main/java/mainPackage/Empresa.java
@@ -11,16 +11,31 @@ package mainPackage;
 public class Empresa {
     private int producao;
     private int max_producao;
+    private int turno;
+    private int max_threads;
 
     public Empresa() {
         producao = 0;
         max_producao = 15;
+        turno = 1;
+        max_threads = 5;
     }
     
     public synchronized boolean trabalhar(int id){
+        while(turno != id){
+            try{
+                wait();
+            }catch(InterruptedException e){
+                
+            }
+        }
         if (producao >= max_producao){
+            turno = (turno+1)%max_threads;
+            notifyAll();
             return false;
         }
+        turno = (turno+1)%max_threads;
+        notifyAll();
         producao++;
         System.out.println("ID: "+ id + " trabalho. " + "Produzido: " + producao);
         return true;
